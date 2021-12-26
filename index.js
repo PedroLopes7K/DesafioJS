58c21a75851c73b794de45a9b9eff92ed1de78ed
Pedro Henrique Lopes <92961068+PedroLopes7K@users.noreply.github.com>
2021-12-26 11:44:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+let Num = '53590'
+let teste2 = '52396789'
+let teste3 = '90123743456539039012345678'
+// [0, 1 ,2,3,4,5,6,7,8,9,0]
+let newArray = []
+let arraySqc = []
+
+function getNum(num) {
+  for (let i = 0; i < num.length; i++) {
+    newArray.push(Number(num[i]))
+  }
+
+  verify(newArray)
+}
+
+function verify(array) {
+  let resultSQC = ''
+  for (let a = 1; a < array.length; a++) {
+    if (array[a - 1] + 1 == array[a] || array[a - 1] - 9 == array[a]) {
+      if (resultSQC == '') {
+        resultSQC = array[a - 1].toString() + array[a].toString()
+      } else {
+        resultSQC += array[a].toString()
+      }
+    } else {
+      if (resultSQC !== '') {
+        arraySqc.push(resultSQC)
+      }
+      resultSQC = ''
+    }
+  }
+  if (resultSQC !== '') {
+    arraySqc.push(resultSQC)
+  }
+  console.log(arraySqc)
+
+  let maior = ''
+  let acount = arraySqc[0]
+  for (let f = 1; f < arraySqc.length; f++) {
+    acount > arraySqc[f] ? (maior = acount) : (maior = arraySqc[f])
+  }
+
+  console.log(maior)
+}
+
+getNum(teste3)
